52f3d4550f68d2f6c4105b1ff7c81284e8bb7b28
Trungdev <trungdev1611@gmail.com>
2022-06-11 10:30:04 +0700

    render details user rely on router param

--- src/controller/homecontroller.js
+++ src/controller/homecontroller.js
@@ -15,15 +15,27 @@ let getHomePage = (req, res) => {
             resultArray = Object.values(JSON.parse(JSON.stringify(results)))
             console.log(resultArray)
             return res.render("test/index.ejs", { dataUser: resultArray })
-
         }
 
-    );
+    )
+}
 
+let getDetailUser = (req, res) => {
+    let { userId } = req.params
+    connection.query(
+        //truy van sql truyen tham so voi nodejs
+        'SELECT * FROM `users` WHERE id = ?', [userId],
+        function (err, results, fields) {
 
+            console.log(results); // results contains rows returned by server
+            return res.render("test/index.ejs", { dataUser: results })
+        }
+
+    )
 }
 
 module.exports = {
-    getHomePage
+    getHomePage,
+    getDetailUser
 }
 
--- src/route/web.js
+++ src/route/web.js
@@ -2,12 +2,14 @@
 import express from 'express';
 import homecontroller from './../controller/homecontroller'
 let router = express.Router()
-
+// noi chi ding cac router tuong ung 
 const initwebRoute = (app) => {
     router.get('/', homecontroller.getHomePage)
     router.get('/about', (req, res) => {
         res.send('Im Trung')
     })
+    router.get('/userdetails/:userId', homecontroller.getDetailUser)
+
     return app.use('/', router)
 }
 
